0624800e59dc40b40f70ec0e25ac94410989d128
Rafael <rafael_breno@outlook.com>
2021-10-31 17:23:09 -0300

    feat: Add custom table name

--- entity/user.go
+++ entity/user.go
@@ -7,11 +7,24 @@ import (
 	"gorm.io/gorm"
 )
 
-type User struct {
+type Tabler interface {
+	TableName() string
+}
+
+type UserModel struct {
 	ID        uuid.UUID `gorm:"primaryKey;default:gen_random_uuid()"`
 	Username  string    `db:"username" json:"username" gorm:"varchar(128);unique;not null"`
 	Password  string    `db:"password" json:"password" gorm:"varchar(128);not null"`
 	CreatedAt time.Time
 	UpdatedAt time.Time
 	DeletedAt gorm.DeletedAt `gorm:"index"`
+}
+
+func (u UserModel) TableName() string {
+	return "user"
+}
+
+type UserRequest struct {
+	Username string `json:"username"`
+	Password string `json:"password"`
 }
